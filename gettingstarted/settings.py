7126a6328c6374b63f630404def552b509928e6e
Kuth <69514175+ezzhop@users.noreply.github.com>
2023-09-16 20:00:27 +0700

    Update database credential

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -122,11 +122,14 @@ if IS_HEROKU_APP:
         # https://devcenter.heroku.com/articles/provisioning-heroku-postgres
         # https://github.com/jazzband/dj-database-url
         DATABASES = {
-            "default": dj_database_url.config(
-                conn_max_age=600,
-                conn_health_checks=True,
-                ssl_require=True,
-            ),
+            "default": {
+                "ENGINE": "django.db.backends.postgresql",
+                "NAME": "daj0h80n2cdkmf",
+                "USER": "gaylxnavvsvxqd",
+                "PASSWORD": "8123ca82a28b07f2e08a6428916a9ebd7d9129d328f3ca5769bf815f33a27008",
+                "HOST": "ec2-3-232-218-211.compute-1.amazonaws.com",
+                "PORT": "5432",
+            }
         }
     else:
         # When running locally in development or in CI, a sqlite database file will be used instead
