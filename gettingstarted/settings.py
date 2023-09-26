2498b9fd3ea8ed5a7f9a0fc5d8aeb459eae8184b
Kuth <69514175+ezzhop@users.noreply.github.com>
2023-09-26 23:24:44 +0700

    Fixed S3 Root

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -326,11 +326,17 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 LOGGING = {
-    # ...
-    "handlers": {
-        "file": {
-            "class": "logging.FileHandler",
-            "filename": "general.log",
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
         },
     },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
 }
+
