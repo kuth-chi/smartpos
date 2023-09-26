d1dda76f1e5ec2e4dbb79182dd0ecc149fdeabf6
Kuth <69514175+ezzhop@users.noreply.github.com>
2023-09-26 23:34:41 +0700

    Fixed S3 Root

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -268,8 +268,9 @@ if IS_HEROKU_APP:
     # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
     # Static files (CSS, JavaScript, etc.)
-    STATIC_URL = 'static/'
+    STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     AWS_S3_OBJECT_PARAMETERS = {
